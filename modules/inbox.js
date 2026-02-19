f7826190c7420624dd01f556e23289eafd95f933
ManuelCer09 <utp0211093@alumno.utpuebla.edu.mx>
2026-02-19 00:56:29 -0600

    commit insano 5.0

--- modules/inbox.js
+++ modules/inbox.js
@@ -719,7 +719,7 @@ export class InboxManager {
             },
             {
                 title: "Enamorarse de ti",
-                content: "<ol style='padding-left: 24px; margin: 0; list-style-position: outside;'><li>Tu forma de ser.</li><li>Tu gusto por las cosas bonitas.</li><li>Tu disciplina y dedicación.</li><li>Tu creatividad.</li><li>Tu risa.</li><li>Tu voz.</li><li>Tu sentido del humor.</li><li>Tu espontaneidad.</li><li>Tu fortaleza.</li><li>Tú.</li></ol>",
+                content: "1. Tu forma de ser.\n2. Tu gusto por las cosas bonitas.\n3. Tu disciplina y dedicación.\n4. Tu creatividad.\n5. Tu risa.\n6. Tu voz.\n7. Tu sentido del humor.\n8. Tu espontaneidad.\n9. Tu fortaleza.\n10. Tú.",
                 type: 'system'
             },
             {
